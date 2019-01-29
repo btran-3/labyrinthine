52c0c568e07e74a26df882696d46c8ed2a5b272c
Brian Tran <btran3@kent.edu>
2019-01-29 13:41:47 -0500

    Begin section 1

--- js/define.js
+++ js/define.js
@@ -1,13 +1,104 @@
 //definition buttons
 
 var
-	showBtn2 = document.querySelector("#test-para button:nth-of-type(1)"),
-	content2 = document.querySelector("#test-para > div");
+	showBtn1 = document.querySelector("#test-para button:nth-of-type(1)"),
+	content1 = document.querySelector("#test-para > div:nth-of-type(1)");
+
+showBtn1.addEventListener("click", function(){
+  if(content1.className == "hidden"){
+	content1.className = "visible";
+} else{
+	content1.className = "hidden";
+}
+}, false);
+
+
+var
+	showBtn2 = document.querySelector("#test-para button:nth-of-type(2)"),
+	content2 = document.querySelector("#test-para > div:nth-of-type(2)");
 
 showBtn2.addEventListener("click", function(){
   if(content2.className == "hidden"){
 	content2.className = "visible";
 } else{
 	content2.className = "hidden";
+}
+}, false);
+
+
+var
+	showBtn3 = document.querySelector("#test-para button:nth-of-type(3)"),
+	content3 = document.querySelector("#test-para > div:nth-of-type(3)");
+
+showBtn3.addEventListener("click", function(){
+  if(content3.className == "hidden"){
+	content3.className = "visible";
+} else{
+	content3.className = "hidden";
+}
+}, false);
+
+
+var
+	showBtn4 = document.querySelector("#test-para button:nth-of-type(4)"),
+	content4 = document.querySelector("#test-para > div:nth-of-type(4)");
+
+showBtn4.addEventListener("click", function(){
+  if(content4.className == "hidden"){
+	content4.className = "visible";
+} else{
+	content4.className = "hidden";
+}
+}, false);
+
+
+var
+	showBtn5 = document.querySelector("#test-para button:nth-of-type(5)"),
+	content5 = document.querySelector("#test-para > div:nth-of-type(5)");
+
+showBtn5.addEventListener("click", function(){
+  if(content5.className == "hidden"){
+	content5.className = "visible";
+} else{
+	content5.className = "hidden";
+}
+}, false);
+
+
+var
+	showBtn6 = document.querySelector("#test-para button:nth-of-type(6)"),
+	content6 = document.querySelector("#test-para > div:nth-of-type(6)");
+
+showBtn6.addEventListener("click", function(){
+  if(content6.className == "hidden"){
+	content6.className = "visible";
+} else{
+	content6.className = "hidden";
+}
+}, false);
+
+
+var
+	showBtn7 = document.querySelector("#test-para button:nth-of-type(7)"),
+	content7 = document.querySelector("#test-para > div:nth-of-type(7)");
+
+showBtn7.addEventListener("click", function(){
+  if(content7.className == "hidden"){
+	content7.className = "visible";
+} else{
+	content7.className = "hidden";
+}
+}, false);
+
+
+var
+	showBtn8 = document.querySelector("#test-para button:nth-of-type(8)"),
+	content8 = document.querySelector("#test-para > div:nth-of-type(8)");
+
+showBtn8.addEventListener("click", function(){
+  if(content8.className == "hidden"){
+	content8.className = "visible";
+} else{
+	content8.className = "hidden";
 }
 }, false);
